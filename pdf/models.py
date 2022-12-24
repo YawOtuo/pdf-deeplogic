9b1a20dcc16150b95b5b98f2635cfbda54e70df1
YawOtuo <yotuo2003@gmail.com>
2022-12-24 02:14:43 +0000

    Adding file path to model definition

--- pdf/models.py
+++ pdf/models.py
@@ -7,5 +7,7 @@ class Text(models.Model):
 
     filename = models.CharField(max_length=80)
     text = models.TextField()
-
+    file_path = models.CharField(max_length=100, blank=True, null=True)
     
+    def __str__(self):
+        return self.filename
